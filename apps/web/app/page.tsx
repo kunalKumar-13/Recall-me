c70bcb213208bc821dd321306fdfcdf5a9ef18d0
Kunal Kumar <kunalsain0324@gmail.com>
2026-07-05 19:25:04 +0530

    feat(web): stats band, how-it-works rows and launcher-states trio

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,4 +1,7 @@
 import { Film } from "./components/v2/Film";
+import { How } from "./components/v2/How";
+import { ScreensTrio } from "./components/v2/ScreensTrio";
+import { Stats } from "./components/v2/Stats";
 import { Hero } from "./components/v2/Hero";
 import { ReadingThread } from "./components/v2/ReadingThread";
 import { SiteNav } from "./components/v2/SiteNav";
@@ -18,7 +21,10 @@ export default function Page() {
       <ReadingThread />
       <main>
         <Hero />
+        <Stats />
         <Film />
+        <How />
+        <ScreensTrio />
       </main>
       <SiteFooter />
     </>
